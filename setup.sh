3a53d77577e8a6cd7a05b29cb66880ae7ee73772
Po Chen <chenpaul914@gmail.com>
2017-01-30 16:37:40 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 set -e
 
-apt-get update
-apt-get install curl git htop zsh
+#apt-get update
+#apt-get install curl git htop zsh
 
-curl -L https://github.com/docker/compose/releases/download/1.10.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+#curl -L https://github.com/docker/compose/releases/download/1.10.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+#chmod +x /usr/local/bin/docker-compose
 
-curl https://raw.githubusercontent.com/princemaple/server-setup/master/setup-zsh.sh | zsh
+#curl https://raw.githubusercontent.com/princemaple/server-setup/master/setup-zsh.sh | zsh
+
+ssh-keygen -t rsa -b 4096 -C "chenpaul914@gmail.com" -N "" -f "~/.ssh/id_rsa"
