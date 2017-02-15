ce44cca16cc570eb812dda281e71cdeeb0754554
Po Chen <chenpaul914@gmail.com>
2017-02-15 22:28:44 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 set -e
 
 apt-get update
-apt-get install curl git htop zsh
+apt-get install -y curl git htop zsh
 
 curl -L https://github.com/docker/compose/releases/download/1.10.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
