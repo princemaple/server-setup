e47c596c409f8b84457631cfb651e06b87eeb2de
Po Chen <chenpaul914@gmail.com>
2017-09-11 15:19:23 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ set -e
 
 apt-get update && apt-get install -y curl git htop zsh
 
-curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 curl https://raw.githubusercontent.com/princemaple/server-setup/master/setup-zsh.sh | zsh
@@ -12,4 +12,5 @@ yes "" | ssh-keygen -t rsa -b 4096 -C "chenpaul914@gmail.com" -N ""
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_rsa
 
-curl -fsSL get.docker.com | sh
+# Leaving it to docker-machine
+# curl -fsSL get.docker.com | sh
