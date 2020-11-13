9afd6f2e0cad17d19a73296c287a7fc421cf8ecc
Po Chen <chenpaul914@gmail.com>
2020-11-13 16:49:55 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ set -e
 
 apt-get update && apt-get install -y curl git htop zsh sudo
 
-curl -L https://github.com/docker/compose/releases/download/1.25.5/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 curl https://raw.githubusercontent.com/princemaple/server-setup/master/setup-zsh.sh | zsh
@@ -17,3 +17,11 @@ ssh-add ~/.ssh/id_rsa
 
 echo "alias dcs='docker-compose -f docker-compose.staging.yml'" | cat >> ~/.zshrc
 echo "alias dcp='docker-compose -f docker-compose.prod.yml'" | cat >> ~/.zshrc
+
+cat << EOF >> ~/.zshrc
+function dexec() {
+  hash=$(docker ps | grep $1 | cut -d ' ' -f 1)
+  echo $1:$hash
+  docker exec -it $hash ${@:2}
+}   
+EOF
