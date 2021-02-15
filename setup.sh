35585cf4e799c6b24be3c2f1af898d6e0a538dd4
Po Chen <chenpaul914@gmail.com>
2021-02-15 23:08:19 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ set -e
 
 apt-get update && apt-get install -y curl git htop zsh sudo
 
-curl -L https://github.com/docker/compose/releases/download/1.27.4/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.28.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 curl https://raw.githubusercontent.com/princemaple/server-setup/master/setup-zsh.sh | zsh
