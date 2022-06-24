57cd1c7587c2a8f1934b8c496f714187dd96b454
Po Chen <chenpaul914@gmail.com>
2022-06-24 16:25:08 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,15 +2,12 @@ set -e
 
 apt-get update && apt-get install -y curl git htop zsh sudo
 
-curl -L https://github.com/docker/compose/releases/download/1.28.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
 curl https://raw.githubusercontent.com/princemaple/server-setup/master/setup-zsh.sh | zsh
 
-yes "" | ssh-keygen -t rsa -b 4096 -C "chenpaul914@gmail.com" -N ""
+yes "" | ssh-keygen -t ed25519 -C "chenpaul914@gmail.com" -N ""
 
 eval $(ssh-agent -s)
-ssh-add ~/.ssh/id_rsa
+ssh-add ~/.ssh/id_ed25519
 
 curl -fsSL get.docker.com | sh
 
